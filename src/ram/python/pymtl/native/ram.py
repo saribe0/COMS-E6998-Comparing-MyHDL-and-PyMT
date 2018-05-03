fd36e3b1bc8b8fc482cffe49d6d04e65bd13ef78
Samuel Beaulieu <sambeau@bu.edu>
2018-05-03 12:01:08 -0400

    Convert tabs to spaces for pymtl

--- src/ram/python/pymtl/native/ram.py
+++ src/ram/python/pymtl/native/ram.py
@@ -2,38 +2,38 @@ from pymtl import *
 
 class ram( Model ):
 
-	# Constructor
-	def __init__( s ):
+    # Constructor
+    def __init__( s ):
 
-		s.DATA_WIDTH = 8
-		s.ADDR_WIDTH = 10
+        s.DATA_WIDTH = 8
+        s.ADDR_WIDTH = 10
 
-		s.data_a = InPort (Bits(s.DATA_WIDTH))
-		s.data_b = InPort (Bits(s.DATA_WIDTH))
+        s.data_a = InPort (Bits(s.DATA_WIDTH))
+        s.data_b = InPort (Bits(s.DATA_WIDTH))
 
-		s.addr_a = InPort (Bits(s.ADDR_WIDTH))
-		s.addr_b = InPort (Bits(s.ADDR_WIDTH))
+        s.addr_a = InPort (Bits(s.ADDR_WIDTH))
+        s.addr_b = InPort (Bits(s.ADDR_WIDTH))
 
-		s.we_a   = InPort (Bits(1))
-		s.we_b   = InPort (Bits(1))
+        s.we_a   = InPort (Bits(1))
+        s.we_b   = InPort (Bits(1))
 
-		s.q_a    = OutPort(Bits(s.DATA_WIDTH))
-		s.q_b    = OutPort(Bits(s.DATA_WIDTH))
+        s.q_a    = OutPort(Bits(s.DATA_WIDTH))
+        s.q_b    = OutPort(Bits(s.DATA_WIDTH))
 
-		s.ram = [Wire(Bits(s.DATA_WIDTH)) for _ in range(2**s.ADDR_WIDTH)]
+        s.ram = [Wire(Bits(s.DATA_WIDTH)) for _ in range(2**s.ADDR_WIDTH)]
 
-		@s.tick_rtl
-		def port_a():
-			if s.we_a:
-				s.ram[s.addr_a].next = s.data_a
-				s.q_a.next 			 = s.data_a
-			else:
-				s.q_a.next = s.ram[s.addr_a]
+        @s.tick_rtl
+        def port_a():
+            if s.we_a:
+                s.ram[s.addr_a].next = s.data_a
+                s.q_a.next           = s.data_a
+            else:
+                s.q_a.next = s.ram[s.addr_a]
 
-		@s.tick_rtl
-		def port_b():
-			if s.we_b:
-				s.ram[s.addr_b].next = s.data_b
-				s.q_b.next 			 = s.data_b
-			else:
-				s.q_b.next = s.ram[s.addr_b]
+        @s.tick_rtl
+        def port_b():
+            if s.we_b:
+                s.ram[s.addr_b].next = s.data_b
+                s.q_b.next           = s.data_b
+            else:
+                s.q_b.next = s.ram[s.addr_b]
--- src/ram/python/pymtl/native/test_ram.py
+++ src/ram/python/pymtl/native/test_ram.py
@@ -9,197 +9,197 @@ ADDR_WIDTH = 10
 
 def test_bench():
 
-	# Local signals
-	test_bench.cycle_ctr	 = Bits(32)
-	test_bench.output_data	 = Bits(280)
-	test_bench.half_word_out = Bits(16)
-	test_bench.byte_out 	 = Bits(8)
+    # Local signals
+    test_bench.cycle_ctr     = Bits(32)
+    test_bench.output_data   = Bits(280)
+    test_bench.half_word_out = Bits(16)
+    test_bench.byte_out      = Bits(8)
 
-	test_bench.we_a = Bits(1)
-	test_bench.we_b = Bits(1)
+    test_bench.we_a = Bits(1)
+    test_bench.we_b = Bits(1)
 
-	test_bench.data_a = Bits(DATA_WIDTH)
-	test_bench.data_b = Bits(DATA_WIDTH)
-	test_bench.addr_a = Bits(ADDR_WIDTH)
-	test_bench.addr_b = Bits(ADDR_WIDTH)
-	test_bench.q_a 	  = Bits(DATA_WIDTH)
-	test_bench.q_b	  = Bits(DATA_WIDTH)
+    test_bench.data_a = Bits(DATA_WIDTH)
+    test_bench.data_b = Bits(DATA_WIDTH)
+    test_bench.addr_a = Bits(ADDR_WIDTH)
+    test_bench.addr_b = Bits(ADDR_WIDTH)
+    test_bench.q_a    = Bits(DATA_WIDTH)
+    test_bench.q_b    = Bits(DATA_WIDTH)
 
-	# Device under test
-	test_bench.model = ram()
-	test_bench.model.elaborate()
-	test_bench.model.DATA_WIDTH = DATA_WIDTH
-	test_bench.model.ADDR_WIDTH = ADDR_WIDTH
-	test_bench.model.we_a = test_bench.we_a
-	test_bench.model.we_b = test_bench.we_b
-	test_bench.model.data_a = test_bench.data_a
-	test_bench.model.data_b = test_bench.data_b
-	test_bench.model.addr_a = test_bench.addr_a
-	test_bench.model.addr_b = test_bench.addr_b
-	test_bench.model.q_a = test_bench.q_a
-	test_bench.model.q_b = test_bench.q_b
+    # Device under test
+    test_bench.model = ram()
+    test_bench.model.elaborate()
+    test_bench.model.DATA_WIDTH = DATA_WIDTH
+    test_bench.model.ADDR_WIDTH = ADDR_WIDTH
+    test_bench.model.we_a = test_bench.we_a
+    test_bench.model.we_b = test_bench.we_b
+    test_bench.model.data_a = test_bench.data_a
+    test_bench.model.data_b = test_bench.data_b
+    test_bench.model.addr_a = test_bench.addr_a
+    test_bench.model.addr_b = test_bench.addr_b
+    test_bench.model.q_a = test_bench.q_a
+    test_bench.model.q_b = test_bench.q_b
 
-	# For simulation
-	test_bench.sim = SimulationTool(test_bench.model)
-	test_bench.sim.reset()
+    # For simulation
+    test_bench.sim = SimulationTool(test_bench.model)
+    test_bench.sim.reset()
 
-	# For verilog translation
-	#vModel = ram()
-	#translated = TranslationTool(vModel)
+    # For verilog translation
+    #vModel = ram()
+    #translated = TranslationTool(vModel)
 
-	def delay(cycles = 1):
-		for _ in range(cycles):
-			test_bench.sim.cycle()
-			test_bench.cycle_ctr.value += 1
+    def delay(cycles = 1):
+        for _ in range(cycles):
+            test_bench.sim.cycle()
+            test_bench.cycle_ctr.value += 1
 
-	def init():
-		test_bench.we_a.value = 0
-		test_bench.we_b.value = 0
-		test_bench.cycle_ctr.value = 0
+    def init():
+        test_bench.we_a.value = 0
+        test_bench.we_b.value = 0
+        test_bench.cycle_ctr.value = 0
 
-	# Write bytes and half words
-	def write_ab(address, data):
-		test_bench.model.addr_a.value = address
-		test_bench.model.addr_b.value = address + 1
-		test_bench.model.data_a.value = data[8 : 16]
-		test_bench.model.data_b.value = data[0 :  8]
-		test_bench.model.we_a.value = 1
-		test_bench.model.we_b.value = 1
+    # Write bytes and half words
+    def write_ab(address, data):
+        test_bench.model.addr_a.value = address
+        test_bench.model.addr_b.value = address + 1
+        test_bench.model.data_a.value = data[8 : 16]
+        test_bench.model.data_b.value = data[0 :  8]
+        test_bench.model.we_a.value = 1
+        test_bench.model.we_b.value = 1
 
-		delay()
+        delay()
 
-		test_bench.model.we_a.value = 0
-		test_bench.model.we_b.value = 0
+        test_bench.model.we_a.value = 0
+        test_bench.model.we_b.value = 0
 
-	def write_a(address, data):
-		test_bench.model.addr_a.value = address
-		test_bench.model.data_a.value = data
-		test_bench.model.we_a.value = 1
+    def write_a(address, data):
+        test_bench.model.addr_a.value = address
+        test_bench.model.data_a.value = data
+        test_bench.model.we_a.value = 1
 
-		delay()
+        delay()
 
-		test_bench.model.we_a.value = 0
+        test_bench.model.we_a.value = 0
 
-	def write_b(address, data):
-		test_bench.model.addr_b.value = address
-		test_bench.model.data_b.value = data
-		test_bench.model.we_b.value = 1
+    def write_b(address, data):
+        test_bench.model.addr_b.value = address
+        test_bench.model.data_b.value = data
+        test_bench.model.we_b.value = 1
 
-		delay()
+        delay()
 
-		test_bench.model.we_b.value = 0
+        test_bench.model.we_b.value = 0
 
-	# Read bytes and half words
-	def read_ab(address):
-		test_bench.model.addr_a.value = address
-		test_bench.model.addr_b.value = address + 1
+    # Read bytes and half words
+    def read_ab(address):
+        test_bench.model.addr_a.value = address
+        test_bench.model.addr_b.value = address + 1
 
-		delay()
+        delay()
 
-		test_bench.half_word_out.value[8 : 16] = test_bench.model.q_a
-		test_bench.half_word_out.value[0 :  8] = test_bench.model.q_b
+        test_bench.half_word_out.value[8 : 16] = test_bench.model.q_a
+        test_bench.half_word_out.value[0 :  8] = test_bench.model.q_b
 
-	def read_a(address):
-		test_bench.model.addr_a.value = address
+    def read_a(address):
+        test_bench.model.addr_a.value = address
 
-		delay()
+        delay()
 
-		test_bench.half_word_out.value[0 : 8] = test_bench.model.q_a
+        test_bench.half_word_out.value[0 : 8] = test_bench.model.q_a
 
-	def read_b(address):
-		test_bench.model.addr_b.value = address
+    def read_b(address):
+        test_bench.model.addr_b.value = address
 
-		delay()
+        delay()
 
-		test_bench.half_word_out.value[0 : 8] = test_bench.model.q_b
-
-
-	# Write and read blocks of 280 bits
-	def write_block(data):
-		write_ab(0,  data[264 : 280])
-		write_ab(2,  data[248 : 264])
-		write_ab(4,  data[232 : 248])
-		write_ab(6,  data[216 : 232])
-		write_ab(8,  data[200 : 216])
-		write_ab(10, data[184 : 200])
-		write_ab(12, data[168 : 184])
-		write_ab(14, data[152 : 168])
-		write_ab(16, data[136 : 152])
-		write_ab(18, data[120 : 136])
-		write_ab(20, data[104 : 120])
-		write_ab(22, data[88  : 104])
-		write_ab(24, data[72  :  88])
-		write_ab(26, data[56  :  72])
-		write_ab(28, data[40  :  56])
-		write_ab(30, data[24  :  40])
-		write_a (32, data[16  :  24])
-		write_a (33, data[8   :  16])
-		write_b (34, data[0   :   8])
-
-	def read_block():
-		read_a (0)
-		test_bench.output_data.value[272 : 280] = test_bench.byte_out
-		read_a (1)
-		test_bench.output_data.value[264 : 272] = test_bench.byte_out
-		read_b (2)
-		test_bench.output_data.value[256 : 264] = test_bench.byte_out
-		read_ab(3)
-		test_bench.output_data.value[240 : 256] = test_bench.half_word_out
-		read_ab(5)
-		test_bench.output_data.value[224 : 240] = test_bench.half_word_out
-		read_ab(7)
-		test_bench.output_data.value[208 : 224] = test_bench.half_word_out
-		read_ab(9)
-		test_bench.output_data.value[192 : 208] = test_bench.half_word_out
-		read_ab(11)
-		test_bench.output_data.value[176 : 192] = test_bench.half_word_out
-		read_ab(13)
-		test_bench.output_data.value[160 : 176] = test_bench.half_word_out
-		read_ab(15)
-		test_bench.output_data.value[144 : 160] = test_bench.half_word_out
-		read_ab(17)
-		test_bench.output_data.value[128 : 114] = test_bench.half_word_out
-		read_ab(19)
-		test_bench.output_data.value[112 : 128] = test_bench.half_word_out
-		read_ab(21)
-		test_bench.output_data.value[96  : 112] = test_bench.half_word_out
-		read_ab(23)
-		test_bench.output_data.value[80  :  96] = test_bench.half_word_out
-		read_ab(25)
-		test_bench.output_data.value[64  :  80] = test_bench.half_word_out
-		read_ab(27)
-		test_bench.output_data.value[48  :  64] = test_bench.half_word_out
-		read_ab(29)
-		test_bench.output_data.value[32  :  48] = test_bench.half_word_out
-		read_ab(31)
-		test_bench.output_data.value[16  :  32] = test_bench.half_word_out
-		read_ab(33)
-		test_bench.output_data.value[0   :  16] = test_bench.half_word_out
+        test_bench.half_word_out.value[0 : 8] = test_bench.model.q_b
 
 
-	# Run the test
-	print ""
-	print "*** Starting RAM test ***"
+    # Write and read blocks of 280 bits
+    def write_block(data):
+        write_ab(0,  data[264 : 280])
+        write_ab(2,  data[248 : 264])
+        write_ab(4,  data[232 : 248])
+        write_ab(6,  data[216 : 232])
+        write_ab(8,  data[200 : 216])
+        write_ab(10, data[184 : 200])
+        write_ab(12, data[168 : 184])
+        write_ab(14, data[152 : 168])
+        write_ab(16, data[136 : 152])
+        write_ab(18, data[120 : 136])
+        write_ab(20, data[104 : 120])
+        write_ab(22, data[88  : 104])
+        write_ab(24, data[72  :  88])
+        write_ab(26, data[56  :  72])
+        write_ab(28, data[40  :  56])
+        write_ab(30, data[24  :  40])
+        write_a (32, data[16  :  24])
+        write_a (33, data[8   :  16])
+        write_b (34, data[0   :   8])
 
-	init()
-	
-	in_data = Bits(280)
-	in_data.value = 0x546869732052414d206d6f64756c652063616e207265616420616e642077726974652e
+    def read_block():
+        read_a (0)
+        test_bench.output_data.value[272 : 280] = test_bench.byte_out
+        read_a (1)
+        test_bench.output_data.value[264 : 272] = test_bench.byte_out
+        read_b (2)
+        test_bench.output_data.value[256 : 264] = test_bench.byte_out
+        read_ab(3)
+        test_bench.output_data.value[240 : 256] = test_bench.half_word_out
+        read_ab(5)
+        test_bench.output_data.value[224 : 240] = test_bench.half_word_out
+        read_ab(7)
+        test_bench.output_data.value[208 : 224] = test_bench.half_word_out
+        read_ab(9)
+        test_bench.output_data.value[192 : 208] = test_bench.half_word_out
+        read_ab(11)
+        test_bench.output_data.value[176 : 192] = test_bench.half_word_out
+        read_ab(13)
+        test_bench.output_data.value[160 : 176] = test_bench.half_word_out
+        read_ab(15)
+        test_bench.output_data.value[144 : 160] = test_bench.half_word_out
+        read_ab(17)
+        test_bench.output_data.value[128 : 114] = test_bench.half_word_out
+        read_ab(19)
+        test_bench.output_data.value[112 : 128] = test_bench.half_word_out
+        read_ab(21)
+        test_bench.output_data.value[96  : 112] = test_bench.half_word_out
+        read_ab(23)
+        test_bench.output_data.value[80  :  96] = test_bench.half_word_out
+        read_ab(25)
+        test_bench.output_data.value[64  :  80] = test_bench.half_word_out
+        read_ab(27)
+        test_bench.output_data.value[48  :  64] = test_bench.half_word_out
+        read_ab(29)
+        test_bench.output_data.value[32  :  48] = test_bench.half_word_out
+        read_ab(31)
+        test_bench.output_data.value[16  :  32] = test_bench.half_word_out
+        read_ab(33)
+        test_bench.output_data.value[0   :  16] = test_bench.half_word_out
 
-	# Write the data to RAM
-	write_block(in_data)
 
-	# Read the data from RAM
-	read_block()
+    # Run the test
+    print ""
+    print "*** Starting RAM test ***"
 
-	# Compare the results
-	if in_data == test_bench.output_data:
-		print "Test Successful"
-	else:
-		print "Recieved 0x%x" % test_bench.output_data
-		print "Expected 0x%x" % in_data
+    init()
+    
+    in_data = Bits(280)
+    in_data.value = 0x546869732052414d206d6f64756c652063616e207265616420616e642077726974652e
 
-	print "\nSimulation took %d clock cycles." % test_bench.cycle_ctr
+    # Write the data to RAM
+    write_block(in_data)
+
+    # Read the data from RAM
+    read_block()
+
+    # Compare the results
+    if in_data == test_bench.output_data:
+        print "Test Successful"
+    else:
+        print "Recieved 0x%x" % test_bench.output_data
+        print "Expected 0x%x" % in_data
+
+    print "\nSimulation took %d clock cycles." % test_bench.cycle_ctr
 
 
 
